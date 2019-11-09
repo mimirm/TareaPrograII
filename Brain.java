65f1f46bb67dc09ca7a944cce48ba161323e72b7
Julianequisde023 <55472888+Julianequisde023@users.noreply.github.com>
2019-11-09 12:08:12 -0600

    Update Brain.java

--- Brain.java
+++ Brain.java
@@ -1,8 +1,9 @@
 public class Brain{
     public static final String MENU = "1.Ver inventario completo.\n2.Ver imagen segun su numero de figura.\n3.Ver imagenes con cantidad de manchas en un rango.\n4.Ver imagenes con escala en un rango.\n5.Ver imagenes que tienen dimensiones en un rango.\n6.Ver imagenes de area en un rango.\n7.Salir.";
+    public static final String MIN = "Digite el minimo para el rango:";
+    public static final String MAX = "Digite el maximo para el rango:";
     public Interfaz interfaz;
     private Inventario inventario;
-    private Catalogo catalogo;
     private Separador separador;
     
     public Brain ( String nombreImagen ){
@@ -37,15 +38,19 @@ public class Brain{
                 //Mostrar una imagen por numero de figura
             break;
             case 3:
-                // Mostrar las imagenes que tienen una cantidad de manchas en un rango
+                inventario.buscarRango(interfaz.askInt(MIN),interfaz.askInt(MAX),1);
+                // Mostrar las imagenes que tienen unas manchas en un rango especifico
             break;
             case 4:
-                // Mostrar las umagenes que tienen una escala en un rango especifico
+                inventario.buscarRango(interfaz.askInt(MIN),interfaz.askInt(MAX),2);
+                // Mostrar las imagenes que tienen una escala en un rango especifico
             break;
             case 5:
+                inventario.buscarDimensiones(interfaz.askInt(MIN),interfaz.askInt(MAX));
                 //Mostrar las imagenes que tienen una dimension original en un rango especifico
             break;
             case 6:
+                inventario.buscarRango(interfaz.askInt(MIN),interfaz.askInt(MAX),3);
                 // Mostrar las imagenes que tienen un area en un rango especifico.
             break;
             case 7:
@@ -59,3 +64,4 @@ public class Brain{
         interfaz.showMessage("Gracias por la visita!");
     }
 }
+
